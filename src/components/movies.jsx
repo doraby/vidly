6d957b6ef16795f4d563f9d4851a79295e8ef125
doraby <doraby1@gmail.com>
2019-02-16 12:34:50 +0300

    Pagination finish

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,11 @@
 import React, {Component } from "react";
 import {getMovies} from "../services/fakeMovieService";
 import {getGenres} from "../services/fakeGenreService";
-import Like from "./common/like";
 import Pagination from "./common/pagination";
 import ListGroup from "./common/listGroup";
 import {paginate} from "../util/paginate";
+import MoviesTable from "./common/moviesTable";
+
 
 export default class SearchList extends Component {
     constructor(props) {
@@ -15,17 +16,18 @@ export default class SearchList extends Component {
             genres:[],
             pageSize: 4,
             currentPage: 1,
-            selectedGenre: {},
+            selectedGenre: undefined,
         };
     }
 
     componentDidMount() {
-        this.setState({allMovies: getMovies(), genres: getGenres()});
+        const genres = [{ name: 'All Genres' }, ...getGenres()];
+        this.setState({ allMovies: getMovies(), genres });
+        console.log(this.state.genres, this.state.selectedGenre ? 'a' : 'b');
     }
 
     handleGenresSelect = (genre) => {
-        this.setState({ selectedGenre: genre })
-        console.log(genre);
+        this.setState({ selectedGenre: genre, currentPage: 1 })
     };
 
     handlePageClick = (selectedPage) =>{
@@ -47,10 +49,13 @@ export default class SearchList extends Component {
 
     render() {
         if (this.state.n === 0) {return <p>There is no results </p>}
-        const {currentPage, pageSize, allMovies} = this.state;
-        const { length: count } = allMovies;
+        const {currentPage, pageSize, allMovies, selectedGenre} = this.state;
 
-        const movies = paginate(allMovies, currentPage, pageSize);
+        const filtered = selectedGenre && selectedGenre._id
+            ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+            : allMovies;
+
+        const movies = paginate(filtered, currentPage, pageSize);
 
         return (
             <div className="row">
@@ -62,42 +67,15 @@ export default class SearchList extends Component {
                     />
                 </div>
                 <div className="col">
-                    <p>Showing {count} results</p>
-                    <table className="table">
-                        <thead>
-                        <tr>
-                            <th scope="col">Title</th>
-                            <th scope="col">Genre</th>
-                            <th scope="col" >Stock</th>
-                            <th scope="col">Rate</th>
-                            <th scope="col"></th>
-                            <th scope="col"></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {movies.map((movie) => (
-                            <tr key={movie._id}>
-                                <td >{movie.title}</td>
-                                <td >{movie.genre.name}</td>
-                                <td >{movie.numberInStock}</td>
-                                <td >{movie.dailyRentalRate}</td>
-                                <td >
-                                    <Like
-                                        onLike={() => this.handleLike(movie)}
-                                        liked={movie.liked}
-                                    />
-                                </td>
-                                <td><button
-                                    onClick={() => this.handleDelete(movie)}
-                                    type="button"
-                                    className="btn btn-danger" >Delete</button></td>
-                            </tr>)
-                        )}
-                        </tbody>
+                    <p>Showing {filtered.length} results</p>
 
-                    </table>
+                    <MoviesTable
+                    movies={movies}
+                    onLike={this.handleLike}
+                    onDelete={this.handleDelete}/>
+
                     <Pagination
-                        countItems={count}
+                        countItems={filtered.length}
                         pageSize={pageSize}
                         currentPage={currentPage}
                         onPage={this.handlePageClick}
