8b233b94a0926fbd3d03dbde85c2c62e2bae6bff
doraby <doraby1@gmail.com>
2019-03-09 13:49:04 +0300

    Pagination finish

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Route, Redirect, Switch} from 'react-router-dom';
+import Customers from "./components/customers";
+import Movies from "./components/movies";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notfound";
+import NavBar from "./components/navbar";
+import MovieForm from "./components/movieForm";
 import './App.css';
-import Searchlist from "./components/movies";
 
 class App extends Component {
-
-
-  render() {
-    return (
-      <main className="container">
-        <h1>результаты поиска</h1>
-      <Searchlist />
-      </main>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar />
+                <main className="container">
+                    <Switch>
+                        <Route path="/movies/:id" component={MovieForm}></Route>
+                        <Route path="/movies" component={Movies}></Route>
+                        <Route path="/customers" component={Customers}></Route>
+                        <Route path="/rentals" component={Rentals}></Route>
+                        <Route path="/not-found" component={NotFound}></Route>
+                        <Redirect from="/" exact to="/movies"/>
+                        <Redirect to="/not-found"/>
+                    </Switch>
+                </main>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -27,7 +27,8 @@ class TableHeader extends Component {
                     { this.props.columns.map(column => (
                         <th
                             className='clickable'
-                            key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>
+                            key={column.path || column.key}
+                            onClick={() => this.raiseSort(column.path)}>
                             {column.label} {this.renderSortIcon(column)}
                         </th>
                     ))}
